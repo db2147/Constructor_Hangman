05192525013f2dbc6b53f498c13667522de46def
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-04 13:06:10 -0600

    adding to main page

--- hangman.js
+++ hangman.js
@@ -1,3 +1,18 @@
 var inquirer = require('inquirer');
 
+//constructor functions
+
+function Word(word) {
+    this.word = word;
+    this.wordArray = this.word.split(' ');
+    this.wordLength = this.word.length;
+    this.drawBoard = function () {
+        var arr = this.wordArray;
+        var start = arr.map(function (val) {
+            return "* ";
+        });
+     
+    }
+}
+
 
