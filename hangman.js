0e6fe788128529088cf15b3080b8abd64013f8a2
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-05 20:33:35 -0600

    changes

--- hangman.js
+++ hangman.js
@@ -1,13 +1,17 @@
 var inquirer = require('inquirer');
 
 var names = require("./word.js") // created a variable requiring my animal names from word.js
-//constructor functions
+
 
 var guessedLetters = [];  //array will hold what we guess
 var guessesLeft = 9; // this is what well use to count down
 
-var wins = 0;  // counter for the wins/losses
-var losses = 0;
+var numWins = 0;  // counter for the wins/losses
+var numLosses = 0;
+var animalLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+//constructor functions
 
 function Word(word) {
     this.word = word;
@@ -42,8 +46,11 @@ function guess() {
         .then(function (val) {
             var n = val.choice;
 
-    // show I have 10 guesses left
 
+    names = function
+    // show I have 10 guesses left
+     
+    
 
     // create a function to get the words
 
@@ -65,7 +72,33 @@ function guess() {
 
         });
 
-        
+      // Function will be called when we reset everything
+      var reset = function() {
+        guessesLeft = 9;
+        guessedLetters = [];
+        updateLetterToGuess();
+        updateGuessesLeft();
+        updateGuessesSoFar();
+      }; 
+      
+       // Execute on page load.
+    updateLetterToGuess();
+    updateGuessesLeft();
+
+
+      
+
+        // If we are out of guesses...
+      if (guessesLeft === 0) {
+
+        losses++;
+
+         // We'll check if there's a match.
+      if (letter === letterToGuess) {
+
+        // If there is then we win and we'll update the HTML to display the win.
+        wins++;
+
 }
 
 
