546634cdf3c765e9b3e9c840e91bf25c75070903
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-07 17:11:24 -0600

    creating

--- hangman.js
+++ hangman.js
@@ -23,16 +23,39 @@ var game = {
 
     },
     startGame: function () {
+        console.log("/n------WELCOME TO HANGMAN--------/n");
+
         console.log(this.renderBoard());
         makeGuess(this);
     },
-        return new Word().wordState();
-        var r = w.map(function (val) {
-            return val.displayState();
-        });
-        return r;
 
-    },
+      evaluateGuess: function (guess) {
+          var w = this.word().wordState();
+          var leng = w.length;
+          for (var i = 0; i < leng; i++) {
+              if (guess === w[i].character) {
+
+            }
+        }
+    }
+}
+game.startGame();
+
+function makeGuess(game) {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "choice",
+            message: "PLEASE GUESS A LETTER"
+        }])
+        .then(function (val) {
+            var n = val.choice;
+            console.log(n);
+            game.evaluateGuess(n);
+            // need to create function to check if guessed letter is correct
+
+        });
+}
 
 
 
