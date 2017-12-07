eb86c4821311d1c027490a5a2b5662b3b84a014d
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-07 17:22:44 -0600

    deleting extra code

--- hangman.js
+++ hangman.js
@@ -58,99 +58,12 @@ function makeGuess(game) {
 }
 
 
+ 
 
 
-}
+ 
 
 
-//constructor functions
-
-function Word(word) {
-    this.word = word;
-    this.wordArray = this.word.split(' ');
-    this.wordLength = this.word.length;
-    this.drawBoard = function () {
-        var arr = this.wordArray;
-        var start = arr.map(function (val) {
-            return "* ";
-        });
-
-        console.log("/nWELCOME TO HANGMAN/n");
-
-        console.log(stars.join(' '));
-        console.log("/n");
-    }
-
-}
-
-// var myWord = new Word("computer");
-
-// guess()
-
-function guess() {
-    myWord.drawBoard();
-    inquirer
-        .prompt([{
-            type: "input",
-            name: "choice",
-            message: "PLEASE GUESS A LETTER"
-        }])
-        .then(function (val) {
-            var n = val.choice;
-
-
-    names = function
-    // show I have 10 guesses left
-     
-    
-
-    // create a function to get the words
-
-
-
-
-
-            // need to create function to check if guessed letter is correct
-        // var guessedLetter = function(){
-        //     validate: function(value){
-        //         if(letter(value)==true)
-        //             return true;
-        //         } else {
-        //             return false;
-        //         }
-        }
-
-
-
-        });
-
-      // Function will be called when we reset everything
-      var reset = function() {
-        guessesLeft = 9;
-        guessedLetters = [];
-        updateLetterToGuess();
-        updateGuessesLeft();
-        updateGuessesSoFar();
-      }; 
-      
-       // Execute on page load.
-    updateLetterToGuess();
-    updateGuessesLeft();
-
-
-      
-
-        // If we are out of guesses...
-      if (guessesLeft === 0) {
-
-        losses++;
-
-         // We'll check if there's a match.
-      if (letter === letterToGuess) {
-
-        // If there is then we win and we'll update the HTML to display the win.
-        wins++;
-
-}
+ 
 
 
